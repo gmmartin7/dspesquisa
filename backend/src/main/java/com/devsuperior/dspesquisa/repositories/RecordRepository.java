7ae9b0bd9a09b4aecf969b207d9613e21ba90f28
Gabriel Martins <gabriel.mg42@outlook.com>
2022-04-24 18:54:57 -0300

    erro

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -1,11 +1,18 @@
 package com.devsuperior.dspesquisa.repositories;
 
+import java.time.Instant;
+import java.util.List;
+
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.devsuperior.dspesquisa.dto.RecordDTO;
 import com.devsuperior.dspesquisa.entities.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+
+	List<RecordDTO> findByMoments(Instant min, Instant max, PageRequest pageRequest);
 
 }

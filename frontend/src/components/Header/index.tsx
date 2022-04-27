4a598830dd109ebf7744b1c2e2509ea6ce27c002
Gabriel Martins <gabriel.mg42@outlook.com>
2022-04-27 15:06:22 -0300

    erro

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import "./styles.css";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import "./style.css";
 
 const Header = () => (
   <header className="main-header">
     <Logo />
-    <div className="logo-test">
-      <span className="Logo-test-1"> BigGame</span>
-      <span className="Logo-test-2"> Survey</span>
+    <div className="Logo-text">
+      <span className="logo-text-1">BigGame</span>
+      <span className="logo-text-2"> Survey</span>
     </div>
   </header>
 );

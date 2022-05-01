a706a5d4b5e834bb6db1fa832e999383dd0d5f96
Gabriel Martins <gabriel.mg42@outlook.com>
2022-05-01 15:43:57 -0300

    atualizando

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Charts from "./assets/pages/Charts/index";
 import Home from "./assets/pages/Home";
 import Records from "./assets/pages/Records/Index";
@@ -8,16 +8,19 @@ import Header from "./components/Header";
 const Routes = () => (
   <BrowserRouter>
     <Header />
-    <Route path="/" exact>
-      <Home />
-    </Route>
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
 
-    <Route path="/Records" exact>
-      <Records />
-    </Route>
-    <Route path="/Charts" exact>
-      <Charts />
-    </Route>
+      <Route path="/Records" exact>
+        <Records />
+      </Route>
+
+      <Route path="/Charts" exact>
+        <Charts />
+      </Route>
+    </Switch>
   </BrowserRouter>
 );
 

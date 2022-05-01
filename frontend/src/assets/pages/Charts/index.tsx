71df754b16a91a5958c3ed6da388797717c824e9
Gabriel Martins <gabriel.mg42@outlook.com>
2022-04-30 21:19:27 -0300

    options

--- frontend/src/assets/pages/Charts/index.tsx
+++ frontend/src/assets/pages/Charts/index.tsx
@@ -1,17 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Filters from "../../../components/Filters/intex";
 import "./style.css";
 import Chart from "react-apexcharts";
-import { barOptions } from "./chart-options";
+import { barOptions, pieOptions } from "./chart-options";
+import axios from "axios";
+import {
+  buildBarSeries,
+  getGenderChartData,
+  getPlatformChartData,
+} from "./helpers";
 
-const chartData = [
-  {
-    x: "Gabriel",
-    y: 10,
-  },
-];
+type PieChartData = {
+  labels: string[];
+  series: number[];
+};
+
+type BarChartData = {
+  x: string;
+  y: number;
+};
+
+const initialPieData = {
+  labels: [],
+  series: [],
+};
+
+const BASE_URL = "https://dspesquisa-martins.herokuapp.com";
 
 const Charts = () => {
+  const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
+  const [platformData, setPlatformData] =
+    useState<PieChartData>(initialPieData);
+  const [genderData, setGenderData] = useState<PieChartData>(initialPieData);
+
+  useEffect(() => {
+    async function getData() {
+      const recordsResponse = await axios.get(`${BASE_URL}/records`);
+      const gamesResponse = await axios.get(`${BASE_URL}/games`);
+      const barData = buildBarSeries(
+        gamesResponse.data,
+        recordsResponse.data.content
+      );
+      setBarChartData(barData);
+      const platformChartData = getPlatformChartData(
+        recordsResponse.data.content
+      );
+      setPlatformData(platformChartData);
+      const genderChartData = getGenderChartData(recordsResponse.data.content);
+      setGenderData(genderChartData);
+    }
+
+    getData();
+  }, []);
+
   return (
     <div className="page-container">
       <Filters link="/records" linkText="VER TABELA" />
@@ -25,16 +66,28 @@ const Charts = () => {
               type="bar"
               width="900"
               height="650"
-              series={[{ data: chartData }]}
+              series={[{ data: barChartData }]}
             />
           </div>
         </div>
         <div className="charts">
           <div className="platform-chart">
             <h2 className="chart-title">Plataformas</h2>
+            <Chart
+              options={{ ...pieOptions, labels: platformData?.labels }}
+              type="donut"
+              series={platformData?.series}
+              width="350"
+            />
           </div>
           <div className="gender-chart">
             <h2 className="chart-title">Gêneros</h2>
+            <Chart
+              options={{ ...pieOptions, labels: genderData.labels }}
+              type="donut"
+              series={genderData?.series}
+              width="350"
+            />
           </div>
         </div>
       </div>
